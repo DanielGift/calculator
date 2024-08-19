d86ec9bd71d955c31f61b75d7acb4860beddbdff
Daniel Gift <daniel.gift@sbs.org>
2024-08-19 17:38:37 -0400

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,22 @@
+#*** prompt the user for the operation to perform
+
+#*** if the operation is valid you should perform the calculation
+
+   #*** prompt the user for the first number
+   
+   #*** prompt the user for the second number
+   
+   #*** perform the calculation based on the inputs provided by the user
+   #*** print out the result
+
+   #*** addition
+   
+   #*** subtraction
+   
+   #*** multiplication
+   
+   #*** division
+   #***   check to make sure the user is not trying to divide by zero
+   #***   and print out that the user is trying to divide by zero
+   
+#*** print out a message to the user if the operation was not valid
